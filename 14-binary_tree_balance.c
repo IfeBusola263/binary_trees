39a8d235ea7a75a73114b537aae1bc0db57601d6
Nazzcodek <bellnas09@gmail.com>
2023-08-31 04:55:14 -0700

    [feature] balance tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,26 +8,36 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	int left_height, right_height;
 
-	if (tree)
-	{
-		left_height = 0;
-		right_height = 0;
-		if (tree->left != NULL)
-		{
-			left_height = 1 + binary_tree_balance(tree->left);
-		}
+	if (tree == NULL)
+		return (0);
 
-		if (tree->right != NULL)
-		{
-			right_height = 1 + binary_tree_balance(tree->right);
-		}
-		/* left_height = binary_tree_height(tree->left); */
-		/* right_height = binary_tree_height(tree->right); */
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-		return (left_height - right_height);
-	}
-	return (0);
+	return (left_height - right_height);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: is a pointer to the root node of the tree to measure the height
+ *
+ * Return: success (0)
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
